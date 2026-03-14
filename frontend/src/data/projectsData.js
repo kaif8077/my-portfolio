1ec5cbb87651d123f5e16a97419e946565f504d7
Mohammad Kaif <mohdkaif90275@gmail.com>
2026-03-14 11:16:33 +0530

    upadte projectsData.js

--- frontend/src/data/projectsData.js
+++ frontend/src/data/projectsData.js
@@ -1,6 +1,6 @@
 import elderlycare from './images/elderlycare.png';
 import portfolio from './images/my_portfolio.png';
-
+import prescripto from './images/prescripto.png';
 
 const projectsData = [
   {
@@ -18,12 +18,21 @@ const projectsData = [
     title: "My Portfolio",
     description: "Personal portfolio showcasing projects, skills, experience, and contact details with a clean responsive design.",
     image: portfolio,
-    technologies: ["React", "JavaScript",  "HTML5 & CSS3","Git & GitHub"],
+    technologies: ["React", "JavaScript", "HTML5 & CSS3", "Git & GitHub"],
     github: "https://github.com/kaif8077/my-portfolio",
     live: "https://my-portfolio-ten-wheat-68.vercel.app",
     category: "Frontend"
   },
-  
+  {
+    id: 3,
+    title: "Prescripto",
+    description: "A full-stack doctor appointment booking platform that allows patients to browse doctors, book appointments, and manage schedules online.",
+    image: prescripto,
+    technologies: ["React", "JavaScript", "Node.js", "Express.js", "MongoDB"],
+    github: "https://github.com/kaif8077/Prescripto",
+    live: "https://prescripto-theta-pied.vercel.app/",
+    category: "Full Stack"
+  }
 ];
 
 export default projectsData;
